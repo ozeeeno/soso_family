5556a1d1fedcf4f69cf17e19fff9050de8c4ab29
Yewon Kim <72730302+noweymik@users.noreply.github.com>
2024-02-01 04:37:33 +0900

    feat: question page

--- frontend/soso_family/lib/pages/question.dart
+++ frontend/soso_family/lib/pages/question.dart
@@ -1,6 +1,5 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class QuestionPage extends StatefulWidget {
   const QuestionPage({Key? key}) : super(key: key);
@@ -15,15 +14,11 @@ class _QuestionPageState extends State<QuestionPage> {
   late String _momAnswer;
   late String _daughterAnswer;
   late String _sonAnswer;
-  final TextEditingController _commentController = TextEditingController();
-
-  // 각 답변에 대한 좋아요 카운트를 저장하는 변수들
+  TextEditingController _commentController = TextEditingController();
   int _dadLikes = 0;
   int _momLikes = 0;
   int _daughterLikes = 0;
   int _sonLikes = 0;
-
-  // 각 답변에 대한 좋아요 여부를 저장하는 변수들
   bool _dadLiked = false;
   bool _momLiked = false;
   bool _daughterLiked = false;
@@ -32,7 +27,6 @@ class _QuestionPageState extends State<QuestionPage> {
   @override
   void initState() {
     super.initState();
-    // 초기 질문과 답안 설정
     _question = "아빠가 가장 중요하게 생각하는 가족과의 소중한 순간이 무엇일까요?";
     _dadAnswer = "가족들과 함께 저녁 먹으면서 대화를 나누는 시간이 가장 소중해";
     _momAnswer = "최근에 일본 오사카로 다 같이 여행가서 놀았던 순간이 아닐까?";
@@ -42,52 +36,62 @@ class _QuestionPageState extends State<QuestionPage> {
 
   @override
   Widget build(BuildContext context) {
+    String formattedDate = DateFormat('yyyy년 MM월 dd일').format(DateTime.now());
+    // Format the current date as needed
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('오늘의 질문'),
+        title: Text('오늘의 질문'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
-            Text(
-              _question,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-            ),
-            const SizedBox(height: 30),
-            buildFamilyMemberAnswer('아빠', _dadAnswer, _dadLikes, _dadLiked),
-            buildFamilyMemberAnswer('엄마', _momAnswer, _momLikes, _momLiked),
-            buildFamilyMemberAnswer(
-                '딸', _daughterAnswer, _daughterLikes, _daughterLiked),
-            buildFamilyMemberAnswer('아들', _sonAnswer, _sonLikes, _sonLiked),
-            const SizedBox(height: 35),
-            const Text(
-              '아들 (나)',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-            const SizedBox(height: 8),
-            TextField(
-              controller: _commentController,
-              decoration: const InputDecoration(
-                hintText: '답변을 작성하세요...',
-                border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 10),
+              Text(
+                ('35번째 질문 $formattedDate'),
+                style: TextStyle(fontSize: 12),
               ),
-              maxLines: 2,
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // 댓글 제출 및 화면 업데이트
-                setState(() {
-                  _sonAnswer = _commentController.text;
-                });
-                print('댓글: $_sonAnswer');
-              },
-              child: const Text('답변 작성'),
-            ),
-          ],
+              SizedBox(height: 10),
+              Text(
+                _question, // Display the question with the formatted date
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              SizedBox(height: 40),
+              buildFamilyMemberAnswer('아빠', _dadAnswer, _dadLikes, _dadLiked),
+              buildFamilyMemberAnswer('엄마', _momAnswer, _momLikes, _momLiked),
+              buildFamilyMemberAnswer(
+                  '딸', _daughterAnswer, _daughterLikes, _daughterLiked),
+              buildFamilyMemberAnswer('아들', _sonAnswer, _sonLikes, _sonLiked),
+              SizedBox(height: 35),
+              Text(
+                '아들 (나)',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              SizedBox(height: 8),
+              TextField(
+                controller: _commentController,
+                decoration: InputDecoration(
+                  hintText: '답변을 작성하세요...',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: 2,
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _sonAnswer = _commentController.text;
+                    _commentController.text = '';
+                  });
+                  print('댓글: $_sonAnswer');
+                },
+                child: Text('답변 작성'),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -103,7 +107,7 @@ class _QuestionPageState extends State<QuestionPage> {
           children: [
             Text(
               member,
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
             ),
             Row(
               children: [
@@ -113,7 +117,6 @@ class _QuestionPageState extends State<QuestionPage> {
                     color: liked ? Colors.red : null,
                   ),
                   onPressed: () {
-                    // 좋아요 버튼 누를 때마다 카운트 증가
                     setState(() {
                       if (member == '아빠') {
                         _dadLikes += liked ? -1 : 1;
@@ -136,12 +139,14 @@ class _QuestionPageState extends State<QuestionPage> {
             ),
           ],
         ),
-        const SizedBox(height: 4),
+        SizedBox(height: 4),
         Text(
           answer,
-          style: const TextStyle(fontSize: 14),
+          style: answer == "아직 답변하지 않았어요.."
+              ? TextStyle(fontSize: 14, color: Colors.grey)
+              : TextStyle(fontSize: 14),
         ),
-        const SizedBox(height: 5),
+        SizedBox(height: 5),
       ],
     );
   }
