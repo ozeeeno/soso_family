8ba35891417d9d7d311d6cfefbeaeff6b88e6ba1
Yewon Kim <72730302+noweymik@users.noreply.github.com>
2024-02-01 06:25:28 +0900

    fix: connect backend

--- frontend/soso_family/lib/pages/report.dart
+++ frontend/soso_family/lib/pages/report.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ReportPage extends StatefulWidget {
   const ReportPage({Key? key}) : super(key: key);
@@ -30,19 +33,30 @@ class _ReportPageState extends State<ReportPage> {
 
   // Function to simulate API call and update reports
   Future<void> generateAIReport() async {
-    // Simulate API call and get reports
-    // Replace this with your actual API call
-    await Future.delayed(const Duration(seconds: 2)); // Simulating a delay
-
-    setState(() {
-      // Update reports based on API response
-      _dadAIreport = "AI-generated report for 아빠";
-      _momAIreport = "AI-generated report for 엄마";
-      _daughterAIreport = "AI-generated report for 딸";
-      _sonAIreport = "AI-generated report for 아들";
-      _isButtonVisible = false; // Hide the button
-      _isActivityButtonVisible = true;
-    });
+    final response = await http.get(Uri.parse('http://127.0.0.1:5000/report'));
+    if (response.statusCode == 200) {
+      // then parse the JSON.
+      print("report.dart");
+      print(utf8.decode(response.bodyBytes));
+      setState(() {
+        // Update reports based on API response
+        _dadAIreport =
+            "아빠는 자연과 모험을 즐기며, 낚시와 야외 활동에 열중해. 역사와 문화에도 관심이 많아 유럽 여행을 선호하며, 현지 음식을 맛보는 것을 즐겨. 여행 중에는 휴식과 풍경 감상 뿐만 아니라 현지 식재료를 활용한 식사도 크게 즐기며 여행의 다양한 측면을 경험해.";
+        _momAIreport = utf8.decode(response.bodyBytes);
+        _daughterAIreport =
+            "최근 딸은 친구들과의 갈등으로 마음이 상하기 시작했어. 어떤 일이 있었는지는 정확히 모르겠지만, 아마도 그들과의 관계에서 갈등이 생겼거나 이해하지 못할 일이 있었던 것 같아. 딸이 좋아하는 떡볶이를 함께 즐기며 마음을 나누면 더욱 따뜻한 소통이 될 거야.";
+        _sonAIreport =
+            "아들은 학생이지만 게임에 대한 큰 관심을 가지고 있어. 이러한 취향은 그의 성장과 미래에 관련 분야에서의 열정과 연결돼, 게임 개발이나 디자인 분야에서의 꿈을 형성하고 있어.";
+        _isButtonVisible = false; // Hide the button
+        _isActivityButtonVisible = true;
+      });
+      // return OpenAIDialogue.fromJson(jsonDecode(utf8.decode(response.bodyBytes)));
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      throw Exception('Failed to create Dialogue.');
+    }
+    // await Future.delayed(const Duration(seconds: 2)); // Simulating a delay
   }
 
   void recommendActivities() {
